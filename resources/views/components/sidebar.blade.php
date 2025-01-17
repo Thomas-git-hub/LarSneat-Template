b7e68a813b1973e1fb528aa4bd702e85c86754df
Thomas-git-hub <thomasallenebonaobra.escoto@bicol-u.edu.ph>
2025-01-17 16:56:23 +0800

    first push

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,84 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+      <a href="#" class="app-brand-link">
+        <img src="" class="app-brand-logo w-px-30 h-auto me-2 " alt="logo" />
+            <span class="app-brand-text menu-text fw-bold">System
+              <br />
+              <span class="fs-tiny fw-medium">System Project</span>
+            </span>
+      </a>
+
+      <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+        <i class="bx bx-chevron-left bx-sm align-middle"></i>
+      </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+         <!-- Dashboards -->
+      <li class="menu-item">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon icon-base bx bx-home-smile"></i>
+          <div data-i18n="Dashboards">Dashboards</div>
+          <div class="badge text-bg-danger rounded-pill ms-auto">5</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item">
+            <a href="dashboards-analytics.html" class="menu-link">
+              <div data-i18n="Analytics">Analytics</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="dashboards-crm.html" class="menu-link">
+              <div data-i18n="CRM">CRM</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-ecommerce-dashboard.html" class="menu-link">
+              <div data-i18n="eCommerce">eCommerce</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-logistics-dashboard.html" class="menu-link">
+              <div data-i18n="Logistics">Logistics</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-academy-dashboard.html" class="menu-link">
+              <div data-i18n="Academy">Academy</div>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <!-- Apps & Pages -->
+      <li class="menu-header small">
+        <span class="menu-header-text" data-i18n="Apps & Pages">Apps &amp; Pages</span>
+      </li>
+      <li class="menu-item">
+        <a href="app-email.html" class="menu-link">
+          <i class="menu-icon icon-base bx bx-envelope"></i>
+          <div data-i18n="Email">Email</div>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="app-chat.html" class="menu-link">
+          <i class="menu-icon icon-base bx bx-chat"></i>
+          <div data-i18n="Chat">Chat</div>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="app-calendar.html" class="menu-link">
+          <i class="menu-icon icon-base bx bx-calendar"></i>
+          <div data-i18n="Calendar">Calendar</div>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="app-kanban.html" class="menu-link">
+          <i class="menu-icon icon-base bx bx-grid"></i>
+          <div data-i18n="Kanban">Kanban</div>
+        </a>
+      </li>
+    </ul>
+  </aside>
